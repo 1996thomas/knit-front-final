218b1c4d78557a0821e1fc830d1667202c1c9bf1
thomas1996 <thomas.kantahck@gmail.com>
2024-04-05 01:23:45 +0200

    adding burger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,55 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.scss";
 import { motion } from "framer-motion";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-  const [shouldRender, setShouldRender] = useState(false);
 
-  const variants = {
+  const sidebarVariants = {
     open: {
-      width: "100vw",
-      transition: { duration: 0.5 },
+      scaleX: 1,
+      transition: { duration: 0.3 },
     },
     closed: {
-      width: 0,
-      transition: { duration: 0.5, when: "afterChildren" },
+      scaleX: 0,
+      transition: { duration: 0.3 },
     },
   };
 
-  useEffect(() => {
-    if (isOpen) setShouldRender(true);
-  }, [isOpen]);
-
-  const onAnimationComplete = () => {
-    if (!isOpen) setShouldRender(false);
+  const linksVariants = {
+    open: {
+      transition: { delay: 0.5, staggerChildren: 0.07, delayChildren: 0.2 },
+    },
+    closed: {
+      transition: { staggerChildren: 0.05, staggerDirection: -1 },
+    },
   };
 
   return (
     <div className="navbar__wrapper">
-      <button onClick={() => setIsOpen(!isOpen)} className="navbar-button">
-        {isOpen ? "Fermer" : "Menu"}
-      </button>
-      {shouldRender && (
-        <motion.div
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className={`center ${isOpen ? "active" : ""}`}
+      ></button>
+      <motion.div
+        initial="closed"
+        animate={isOpen ? "open" : "closed"}
+        variants={sidebarVariants}
+        className="fullscreen-navbar"
+        style={{ originX: isOpen ? 1 : 0 }}
+      >
+        <motion.nav
+          variants={linksVariants}
           initial="closed"
           animate={isOpen ? "open" : "closed"}
-          variants={variants}
-          className="fullscreen-navbar"
-          onAnimationComplete={onAnimationComplete} // Appelé lorsque l'animation est complète
-          style={{ originX: 0, originY: 0 }}
         >
-          <nav>
-            <Link onClick={() => setIsOpen(false)} to={"/"}>
-              Home
-            </Link>
-            <Link onClick={() => setIsOpen(false)} to={"/article"}>
-              Articles
-            </Link>
-          </nav>
-        </motion.div>
-      )}
+          <Link onClick={() => setIsOpen(false)} to={"/"}>
+            Home
+          </Link>
+          <Link onClick={() => setIsOpen(false)} to={"/article"}>
+            Articles
+          </Link>
+        </motion.nav>
+      </motion.div>
     </div>
   );
 }
--- src/utils/transition/transition.jsx
+++ src/utils/transition/transition.jsx
@@ -10,13 +10,13 @@ const transition = (Component) => {
         initial={{ scaleY: 0 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 1 }}
-        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+        transition={{ delay: .3, duration: 1, ease: [0.22, 1, 0.36, 1] }}
       >
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 0 }}
           exit={{ opacity: 1 }}
-          transition={{ duration: 1 }}
+          transition={{ duration: 1, delay:.3 }}
         >
           Coucou
         </motion.div>
@@ -26,7 +26,7 @@ const transition = (Component) => {
         initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 0 }}
-        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+        transition={{ delay: .3, duration: .7, ease: [0.22, 1, 0.36, 1] }}
       ></motion.div>
     </>
   );

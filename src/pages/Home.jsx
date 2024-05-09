b3a8b8320dfd35385925f669a9a1adb1b4b2c763
thomas1996 <thomas.kantahck@gmail.com>
2024-05-09 17:26:31 +0200

    add video mp4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,24 +7,21 @@ const Home = () => {
         <video autoPlay loop muted src="/lol.mp4"></video>
       </div>
       <div className="about__wrapper">
-        <div className="first">
-          <h2>A propos de KNIT</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque vel
-            delectus minus blanditiis ad aliquid corporis adipisci officiis,
-            molestiae deleniti, temporibus obcaecati tempora, ratione quisquam
-            exercitationem fugit perspiciatis ullam cupiditate.
-          </p>
-        </div>
-        <div className="second">
-          <h2>Notre vision de la culture</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque vel
-            delectus minus blanditiis ad aliquid corporis adipisci officiis,
-            molestiae deleniti, temporibus obcaecati tempora, ratione quisquam
-            exercitationem fugit perspiciatis ullam cupiditate.
-          </p>
-        </div>
+        <h1>BIENVENUE</h1>
+        <h2>A propos</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque vel
+          delectus minus blanditiis ad aliquid corporis adipisci officiis,
+          molestiae deleniti, temporibus obcaecati tempora, ratione quisquam
+          exercitationem fugit perspiciatis ullam cupiditate.
+        </p>
+        <h2>Notre vision de la culture</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque vel
+          delectus minus blanditiis ad aliquid corporis adipisci officiis,
+          molestiae deleniti, temporibus obcaecati tempora, ratione quisquam
+          exercitationem fugit perspiciatis ullam cupiditate.
+        </p>
       </div>
     </div>
   );

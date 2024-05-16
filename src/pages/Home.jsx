26b12df511ee7bfa698b9af13031b00adf65d672
thomas1996 <thomas.kantahck@gmail.com>
2024-05-16 12:49:05 +0200

    lighter video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,35 +5,11 @@ import "./home.scss";
 import { useState } from "react";
 import { getArticles } from "../utils/apiCalls";
 const Home = () => {
-  const [articles, setArticles] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [sponsoredArticles, setSponsoredArticles] = useState([]);
-  useEffect(() => {
-    getArticles().then((responseData) => {
-      const reverseArray = responseData.data.slice().reverse();
-      setArticles(reverseArray);
-      const sponsored = reverseArray.filter(
-        (article) => article.attributes.sponsored
-      );
-      setSponsoredArticles(sponsored);
-      setIsLoading(false);
-    });
-  }, []);
-
   return (
     <div className="home">
       <div className="home__video">
-        <video playsInline autoPlay muted src="/lol.mp4"></video>
+        <video playsInline autoPlay loop muted src="/lol.mp4"></video>
       </div>
-      <div className="home-articles">
-        <h2>TOP ARTICLES</h2>
-        <SwipeCarousel articles={sponsoredArticles} />
-      </div>
-      <div className="home-articles">
-        <h2>SHOP</h2>
-        <SwipeCarousel articles={sponsoredArticles} />
-      </div>
-
     </div>
   );
 };

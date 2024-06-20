c28e514093230767d48de4d2c703a5ea42b1935d
thomas1996 <thomas.kantahck@gmail.com>
2024-06-20 11:57:13 +0200

    fix insta error

--- src/components/ParallaxImage.jsx
+++ src/components/ParallaxImage.jsx
@@ -9,20 +9,20 @@ const ParallaxImage = ({ src, alt, className, figCaption }) => {
   const imageRef = useRef(null);
   const containerRef = useRef(null);
 
-  useEffect(() => {
-    if (imageRef.current && containerRef.current) {
-      gsap.to(imageRef.current, {
-        yPercent: -12,
-        ease: "power1.out",
-        scrollTrigger: {
-          trigger: containerRef.current,
-          start: "top bottom",
-          end: "bottom top",
-          scrub: true,
-        },
-      });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (imageRef.current && containerRef.current) {
+  //     gsap.to(imageRef.current, {
+  //       yPercent: -12,
+  //       ease: "power1.out",
+  //       scrollTrigger: {
+  //         trigger: containerRef.current,
+  //         start: "top bottom",
+  //         end: "bottom top",
+  //         scrub: true,
+  //       },
+  //     });
+  //   }
+  // }, []);
 
   return (
     <div ref={containerRef} className="parallax-container">
--- src/utils/InstagramEmbed.jsx
+++ src/utils/InstagramEmbed.jsx
@@ -1,12 +1,7 @@
 import "./InstagramEmbed.scss";
 import { InstagramEmbed } from "react-social-media-embed";
 const InstagramEmbedDiv = ({ url }) => {
-  return (
-    <InstagramEmbed
-      url="https://www.instagram.com/p/CUbHfhpswxt/"
-      width={328}
-    />
-  );
+  return <InstagramEmbed url={url} width={328} />;
 };
 
 export default InstagramEmbedDiv;

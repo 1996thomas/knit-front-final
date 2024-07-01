6d7d14c5abc35e5c749d61497cb101008ddaeb2e
thomas1996 <thomas.kantahck@gmail.com>
2024-07-01 08:13:52 +0200

    add legal to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,27 @@
+import { Link } from "react-router-dom";
 import ContactForm from "./ContactForm";
 import RSComp from "./RSComp";
-import './footer.scss';
+import "./footer.scss";
 
 export default function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
   return (
-    <div className="footer__wrapper">
-      <div className="rs__wrapper">
-          <RSComp color={"white"} flexDirection={"row"}/>
+    <>
+      <div className="footer__wrapper">
+        <div className="rs__wrapper">
+          <RSComp color={"white"} flexDirection={"row"} />
+        </div>
+        <span className="footer--separator" />
+        <div className="contact__wrapper">
+          <ContactForm />
+        </div>
       </div>
-      <span className="footer--separator" />
-      <div className="contact__wrapper">
-        <ContactForm />
+      <div className="link">
+        <p>
+          © {year} - KNIT<Link to={"/legal"}>Mentions légales</Link>
+        </p>
       </div>
-    </div>
+    </>
   );
 }

4b6963415b0d73151853f11b84f5e8af17eb2af5
thomas1996 <thomas.kantahck@gmail.com>
2024-07-17 16:09:14 +0200

    add seo image

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -8,9 +8,16 @@ export default function SEO({ title, description, name, type }) {
       <meta name="description" content={description} />
       {/* End standard metadata tags */}
       {/* Facebook tags */}
-      <meta property="og:type" content={type} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta
+        property="og:image"
+        content="https://media.istockphoto.com/id/1443562748/fr/photo/mignon-chat-gingembre.jpg?s=612x612&w=0&k=20&c=ygNVVnqLk9V8BWu4VQ0D21u7-daIyHUoyKlCcx3K1E8="
+      ></meta>
+      <meta property="og:url" content="https://www.99knit.com"></meta>
+      <meta property="og:site_name" content="Test"></meta>
+      <meta property="og:type" content={type} />
+
       {/* End Facebook tags */}
       {/* Twitter tags */}
       <meta name="twitter:creator" content={name} />

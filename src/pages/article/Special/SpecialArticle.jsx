01452d2db6c295f8ca6586a5f0bb52ce3b8449aa
thomas1996 <thomas.kantahck@gmail.com>
2024-08-27 13:36:38 +0200

    add tiny jpg

--- src/pages/article/Special/SpecialArticle.jsx
+++ src/pages/article/Special/SpecialArticle.jsx
@@ -1,79 +1,60 @@
 import React, { useEffect, useRef } from "react";
 import "./special-article.scss";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import QuestionResponse from "./QuestionResponse";
 import { data } from "./data";
-import { useLayoutEffect } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function SpecialArticle() {
-  const loaderRef = useRef(null);
-
-  // useEffect(() => {
-  //   const minimumLoadingTime = 500; // in milliseconds
-  //   const startTime = Date.now();
-  //   console.log(data);
-
-  //   window.addEventListener("load", () => {
-  //     const timeElapsed = Date.now() - startTime;
-  //     const remainingTime = minimumLoadingTime - timeElapsed;
-  //     const delay = remainingTime > 0 ? remainingTime : 0;
-  //     setTimeout(() => {
-  //       if (loaderRef.current) {
-  //         gsap.to(loaderRef.current, {
-  //           opacity: 0,
-  //           duration: 1,
-  //           ease: "power2.out",
-  //           onComplete: () => {
-  //             if (loaderRef.current) {
-  //               loaderRef.current.style.display = "none";
-  //             }
-  //           },
-  //         });
-  //       }
-  //     }, delay);
-  //   });
-  // }, []);
+  const heroRef = useRef(null);
+  const heroTitleRef = useRef(null);
 
   useEffect(() => {
-    // ANIMATION HERO
-    gsap.to(".hero", {
-      yPercent: -20,
-      transform: "scale(0.2)",
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".hero",
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-      },
-    });
+    const heroElement = heroRef.current;
+    const heroTitleElement = heroTitleRef.current;
 
-    gsap.to(".hero > h1", {
-      yPercent: -5,
-      // scale: 2,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".hero",
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-      },
-    });
+    if (heroElement && heroTitleElement) {
+      // ANIMATION HERO
+      gsap.to(heroElement, {
+        yPercent: -20,
+        scale: 0.8,
+        ease: "none",
+        scrollTrigger: {
+          trigger: heroElement,
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+          debounce: 1, // Ajout du debounce pour améliorer la performance
+        },
+      });
+
+      gsap.to(heroTitleElement, {
+        yPercent: -5,
+        ease: "none",
+        scrollTrigger: {
+          trigger: heroElement,
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+          debounce: 1, // Ajout du debounce pour améliorer la performance
+        },
+      });
+    }
   }, []);
 
   return (
     <div className="special-article__wrapper">
-      {/* <div className="loader" ref={loaderRef}>
-        <div className="loader--heading">
-          <img src="/KNIT_WHITE.png" />
-          <p>présente</p>
-          <h1>MONSIEUR BONHEUR</h1>.
-        </div>
-        <img src="/50.JPG" alt="" />
-      </div> */}
       <div className="container">
-        <section className="hero">
-          <h1>Le visage des oublié-e-s</h1>
+        <section
+          className="hero"
+          ref={heroRef}
+          style={{ willChange: "transform" }}
+        >
+          <h1 ref={heroTitleRef} style={{ willChange: "transform" }}>
+            Le visage des oublié-e-s
+          </h1>
           <div className="hero--info">
             <span>© Marvin Bonheur</span>
             <span>Interview par Paul-Louis Godier</span>
@@ -81,13 +62,22 @@ export default function SpecialArticle() {
             <span>Réalisé par KNIT</span>
           </div>
         </section>
-        <section className="info"></section>
+        <section className="info">
+          <div className="intro--carousel">
+            <img src="/1.JPG" alt="" />
+            <img src="/1a.JPG" alt="" />
+            <img src="/93e vague.JPG" alt="" />
+          </div>
+          <div className="intro">
+            <p>SALUT</p>
+          </div>
+        </section>
       </div>
       <div className="question-reponse__wrapper">
         {data.map((item, index) => (
           <QuestionResponse
             key={index}
-            uniqueId={index} // Ajouter un identifiant unique
+            uniqueId={index}
             reponse={item.reponse}
             question={item.question}
             imgsSrc={item.imgsSrc}
